60d9e556902f9714711f9feae1fafa5595afd1f7
Brent Doering <brentiscoding@gmail.com>
2023-07-24 16:38:04 -0600

    project upload

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+
+export default function AboutPage() {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>This is an app to search local DJ and other music events in Denver, Colorado.</p>
+        <p>Version: 1.0.0</p>
+    </div>
+  )
+}
