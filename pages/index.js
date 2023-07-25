6da4c5144e85b115813a1154fd2567468d10a125
Brent Doering <brentiscoding@gmail.com>
2023-07-24 18:07:49 -0600

    create map info with EventItem component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 import Layout from '@/components/Layout'
 import { API_URL } from '@/config'
+import EventItem from '@/components/EventItem'
 
 export default function HomePage({events}) {
   console.log(events)
   return (
     <Layout>
      <h1>Upcoming Events</h1>
+     {events.length === 0 && <h3>No events to show</h3>}
+
+     {events.map(evt => (
+      <EventItem key={evt.id} evt={evt} />
+     ))}
     </Layout>
   )
 }
